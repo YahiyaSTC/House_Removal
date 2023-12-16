7bcd38e7ca20823d6fdc401a8f2ee7bbad4ce77b
YahiyaSTC <yahiyastc@gmail.com>
2023-12-16 16:05:09 +0500

    changess

---
+++ frontend/assets/js/owl.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  $(".owwl").owlCarousel({
+    items: 5,
+    center: true,
+    autoplay: true,
+    autoPlayTimeout: 0,
+    autoplaySpeed: 9000,
+    autoHeight: true,
+    autoplayHoverPause: false,
+    loop: true,
+    margin: 20,
+  });
+});
